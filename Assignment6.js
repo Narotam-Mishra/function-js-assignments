32800969c84a0167469f41ddb431175eaebf50f3
Narotam-Mishra <narotam21jan@gmail.com>
2023-12-05 01:46:40 +0530

    added Assignment6 solution

--- Assignment6.js
+++ Assignment6.js
@@ -5,10 +5,22 @@ function until all arguments are provided. Then, it should execute the original 
 Test the curry function with a function that adds two numbers.
 */
 
-function curry(n1){
-    return function(n2){
-        return n1+n2;
+function curry(fn){
+    
+    return function curried(...args){
+        if(args.length >= fn.length){
+            return fn(...args);
+        }else{
+            return function(...nextArgs){
+                return curried(...args, ...nextArgs);
+            }
+        }
     }
 }
 
-console.log("Sum using curry function:",curry(12)(23));
+function sum(n1,n2){ return n1+n2 }
+
+const cSum = curry(sum);
+
+console.log("Curried sum: ",cSum(12)(10));
+console.log("Curried sum: ",cSum(15,11));
